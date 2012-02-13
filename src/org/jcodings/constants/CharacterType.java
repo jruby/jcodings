66144debdaddeaf04fcbcdb6746e13bb5db01a09
Marcin Mielzynski <lopx@gazeta.pl>
2012-02-13 01:35:28 +0100

    special mask for \s \d \w

--- src/org/jcodings/constants/CharacterType.java
+++ src/org/jcodings/constants/CharacterType.java
@@ -1,20 +1,20 @@
 /*
- * Permission is hereby granted, free of charge, to any person obtaining a copy of 
- * this software and associated documentation files (the "Software"), to deal in 
- * the Software without restriction, including without limitation the rights to 
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
  * of the Software, and to permit persons to whom the Software is furnished to do
  * so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
 package org.jcodings.constants;
@@ -35,9 +35,15 @@ public interface CharacterType {
     final int WORD      = 12;
     final int ALNUM     = 13;      /* alpha || digit */
     final int ASCII     = 14;
-    
+
+    final int SPECIAL_MASK = 256;
+    final int S = SPECIAL_MASK | SPACE;
+    final int D = SPECIAL_MASK | DIGIT;
+    final int W = SPECIAL_MASK | WORD;
+
+
     final int MAX_STD_CTYPE = 14;
-    
+
     final int BIT_NEWLINE  = (1<< NEWLINE);
     final int BIT_ALPHA    = (1<< ALPHA);
     final int BIT_BLANK    = (1<< BLANK);
@@ -53,5 +59,5 @@ public interface CharacterType {
     final int BIT_WORD     = (1<< WORD);
     final int BIT_ALNUM    = (1<< ALNUM);
     final int BIT_ASCII    = (1<< ASCII);
-    
+
 }
