7258cb78e01c7ab615b58d33595f059eea1881d4
Marcin Mielzynski <lopx@gazeta.pl>
2012-02-14 03:32:08 +0100

    Make the script look for code range aliases as well. Update code range tables.

--- scripts/ExtractUnicodeTables.rb
+++ scripts/ExtractUnicodeTables.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 enc_path = "http://svn.ruby-lang.org/repos/ruby/branches/ruby_1_9_3/enc"
 folds_src = open("#{enc_path}/unicode.c").read
 unicode_src = open("#{enc_path}/unicode/name2ctype.src").read
+
 dest_dir = "../src/org/jcodings/unicode/"
 INDENT = " " * 4
 
@@ -26,10 +27,35 @@ end
 
 cr_map =  unicode_src.scan(/#define (CR_.*?) (.*)/).inject(Hash.new{|h, k|k}){|h, (k,v)| h[k] = v;h}
 
+def convert_property name
+    n = name[/CR_(.*)/, 1]
+    case n
+        when /Age_(\d)_(\d)/
+            "age=#{$1}.#{$2}"
+        else
+            n.tr('_', '').downcase
+    end
+end
+
+aliases = unicode_src[/%%(.*?)%%/m, 1].scan(/(.*?),\s+(\d+)/).inject(Hash.new{|h, k| h[k] = []}){|h, (name, num)| h[num.to_i] << name; h}.inject({}) do |h, (k, v)|
+    full, *abbr = v.map{|e|e.strip}
+    h[full] = abbr
+    h
+end
+
 unicode_src.scan(/CodeRanges\[\]\s+=\s+\{(.*?)\}\;/m) do |e|
     names = e.first.scan(/CR_\w+/)
-    crs = names.map{|c|cr_map[c]}.map{|c|"#{INDENT * 4}#{c}.Table"}
-    cnames = names.map{|n| "#{INDENT * 4}\"#{n[/CR_(.*)/, 1]}\".getBytes()"}
+
+    crs = names.map do |c|
+        px = convert_property(c)
+        ([c] * (aliases[px].to_a.length + 1)).map{|c|"#{INDENT * 4}#{cr_map[c]}.Table"}.join(",\n")
+    end
+
+    cnames = names.map do |n|
+       px = convert_property(n)
+       ([px] + aliases[px].to_a).map{|n| "#{INDENT * 4}\"#{n}\".getBytes()"}.join(",\n")
+    end
+
     open("#{dest_dir}/UnicodeProperties.java", "wb"){|f| f << open("UnicodePropertiesTemplate.java", "rb").read.
         sub(/%\{stdcrs\}/, crs[0..14].join(",\n")).
         sub(/%\{extcrs\}/, crs.join(",\n")).
