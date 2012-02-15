c38f70cd5f41e21cafe6799e10e16fed11d8e165
Marcin Mielzynski <lopx@gazeta.pl>
2012-02-15 20:06:26 +0100

    GB2312 is implemented via EUCKR, make unicode properties case insensitive.

--- src/org/jcodings/EncodingDB.java
+++ src/org/jcodings/EncodingDB.java
@@ -249,7 +249,7 @@ public class EncodingDB {
         "UTF-32BE",     "UTF32BE",
         "UTF-32LE",     "UTF32LE",
         "Windows-1251", "CP1251",
-        "GB2312",       "GB2312"         // should be done via rb_enc_register
+        "GB2312",       "EUCKR"         // done via rb_enc_register
     };
 
     static Entry ascii;
--- src/org/jcodings/unicode/UnicodeProperties.java
+++ src/org/jcodings/unicode/UnicodeProperties.java
@@ -1,26 +1,26 @@
 /*
- * Permission is hereby granted, free of charge, to any person obtaining a copy of 
- * this software and associated documentation files (the "Software"), to deal in 
- * the Software without restriction, including without limitation the rights to 
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
  * of the Software, and to permit persons to whom the Software is furnished to do
  * so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
 package org.jcodings.unicode;
 
 import org.jcodings.Config;
-import org.jcodings.util.BytesHash;
+import org.jcodings.util.CaseInsensitiveBytesHash;
 
 
 public class UnicodeProperties {
@@ -838,12 +838,12 @@ public class UnicodeProperties {
                 "ascii".getBytes()
             };
 
-    private static BytesHash<Integer> initializeCTypeNameTable() {
-        BytesHash<Integer> table = new BytesHash<Integer>();
-        for (int i=0; i<CTypeNameTable.length; i++) table.putDirect(CTypeNameTable[i], i);
+    private static CaseInsensitiveBytesHash<Integer> initializeCTypeNameTable() {
+        CaseInsensitiveBytesHash<Integer> table = new CaseInsensitiveBytesHash<Integer>();
+        for (int i = 0; i < CTypeNameTable.length; i++) table.putDirect(CTypeNameTable[i], i);
 
         return table;
     }
-    
-    static final BytesHash<Integer> CTypeNameHash = initializeCTypeNameTable();
+
+    static final CaseInsensitiveBytesHash<Integer> CTypeNameHash = initializeCTypeNameTable();
 }
