0af3c4d73efc5d52df8298b5106f9e299aa29c7a
Marcin Mielzynski <lopx@gazeta.pl>
2017-12-15 09:13:31 +0100

    add test for unicode property

--- test/org/jcodings/specific/TestUnicode.java
+++ test/org/jcodings/specific/TestUnicode.java
@@ -1,5 +1,6 @@
 package org.jcodings.specific;
 
+import org.jcodings.Encoding;
 import org.junit.Test;
 
 import static junit.framework.Assert.*;
@@ -13,5 +14,15 @@ public class TestUnicode {
         assertEquals(5, UTF8Encoding.INSTANCE.strLength(utf8Bytes, 0, 7));
         assertEquals(2, UTF8Encoding.INSTANCE.length(utf8Bytes[1]));
         assertEquals('Ø', UTF8Encoding.INSTANCE.mbcToCode(utf8Bytes, 1, 3));
+    }
+
+    @Test
+    public void testUnicodeProperties() throws Exception {
+        Encoding enc = UTF16BEEncoding.INSTANCE;
+        byte[]str = "\000B\000\000".getBytes("iso-8859-1");
+        int code = enc.mbcToCode(str, 0, str.length);
+        byte[]prop = "\000u\000p\000p\000e\000r".getBytes("iso-8859-1");
+        int ctype = enc.propertyNameToCType(prop, 0, prop.length);
+        assertTrue(enc.isCodeCType(code, ctype));
     }
 }
