4196dba276d13de389ede96b93dbd5cd233e2362
Marcin Mielzynski <lopx@gazeta.pl>
2018-02-13 17:21:11 +0100

    add euc-jp code ranges

--- src/org/jcodings/specific/BaseEUCJPEncoding.java
+++ src/org/jcodings/specific/BaseEUCJPEncoding.java
@@ -20,7 +20,6 @@
 package org.jcodings.specific;
 
 import org.jcodings.CodeRange;
-import org.jcodings.Config;
 import org.jcodings.EucEncoding;
 import org.jcodings.IntHolder;
 import org.jcodings.ascii.AsciiTables;
@@ -95,25 +94,60 @@ abstract class BaseEUCJPEncoding extends EucEncoding {
     private static final int CR_Hiragana[] = {
         1,
         0xa4a1, 0xa4f3
-    }; /* CR_Hiragana */
+    };
 
     private static final int CR_Katakana[] = {
         3,
+        0x8ea6, 0x8eaf,   /* JIS X 0201 Katakana */
+        0x8eb1, 0x8edd,   /* JIS X 0201 Katakana */
         0xa5a1, 0xa5f6,
-        0xaaa6, 0xaaaf,
-        0xaab1, 0xaadd
-    }; /* CR_Katakana */
+    };
+
+    private static final int CR_Han[] = {
+        /* EUC-JP (JIS X 0208 based) */
+        4,
+        0xa1b8, 0xa1b8,
+        0xb0a1, 0xcfd3,       /* Kanji level 1 */
+        0xd0a1, 0xf4a6,       /* Kanji level 2 */
+        0x8fb0a1, 0x8fedf3    /* JIS X 0212 Supplemental Kanji (row 16 .. 77) */
+    };
+
+    private static final int CR_Latin[] = {
+        4,
+        0x0041, 0x005a,
+        0x0061, 0x007a,
+        0xa3c1, 0xa3da,
+        0xa3e1, 0xa3fa,
+    };
+
+    private static final int CR_Greek[] = {
+        2,
+        0xa6a1, 0xa6b8,
+        0xa6c1, 0xa6d8,
+    };
+
+    private static final int CR_Cyrillic[] = {
+        2,
+        0xa7a1, 0xa7c1,
+        0xa7d1, 0xa7f1,
+    };
 
     private static final int PropertyList[][] = new int[][] {
         CR_Hiragana,
-        CR_Katakana
+        CR_Katakana,
+        CR_Han,
+        CR_Latin,
+        CR_Greek,
+        CR_Cyrillic
     };
 
     private static final CaseInsensitiveBytesHash<Integer> CTypeNameHash = new CaseInsensitiveBytesHash<Integer>();
 
     static {
-        CTypeNameHash.put("Hiragana".getBytes(), 1 + CharacterType.MAX_STD_CTYPE);
-        CTypeNameHash.put("Katakana".getBytes(), 2 + CharacterType.MAX_STD_CTYPE);
+        String[] names = new String[] {"Hiragana", "Katakana", "Han", "Latin", "Greek", "Cyrillic"};
+        for (int i = 0; i < names.length; i++) {
+            CTypeNameHash.put(names[i].getBytes(), i + 1 + CharacterType.MAX_STD_CTYPE);
+        }
     }
 
     @Override
