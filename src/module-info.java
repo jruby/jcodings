9ee52a223af1160bf52f9475dddc37c795c2e837
Charles Oliver Nutter <headius@headius.com>
2018-09-05 23:00:54 -0500

    Add module-info at root to support Java 9 modules.

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+open module org.jruby.jcodings {
+    exports org.jcodings;
+    exports org.jcodings.ascii;
+    exports org.jcodings.constants;
+    exports org.jcodings.exception;
+    exports org.jcodings.specific;
+    exports org.jcodings.spi;
+    exports org.jcodings.transcode;
+    exports org.jcodings.transcode.specific;
+    exports org.jcodings.unicode;
+    exports org.jcodings.util;
+}
